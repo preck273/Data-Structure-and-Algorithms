aa9358692b8b692d5c2c810b4608c408867e4236
preck273 <72576947+preck273@users.noreply.github.com>
2023-03-28 19:05:16 +0200

    Test hashMap in main

--- src/com/dataStructure/Main.java
+++ src/com/dataStructure/Main.java
@@ -1,7 +1,8 @@
 package com.dataStructure;
 
+import HashMap.CustomizedHashMap;
 import LinkedList.LinkedListCustomized;
-import java.util.Arrays;
+
 import java.util.Iterator;
 
 public class Main {
@@ -11,7 +12,28 @@ public class Main {
         ArrayListCustomised namer = new ArrayListCustomised(10);
         Integer[] names = {1,3,4,2,7,3,11,9,7,2,10};
 
-        namer.addall(names);
+
+        CustomizedHashMap<String, String> map = new CustomizedHashMap<String, String>();
+        map.put("A", "B");
+        map.put("E", "F");
+        map.put("H", "P");
+        map.put("P", "2");
+        map.put("1",  "G");
+        map.put("2", "6");
+        map.put("3", "2");
+        map.put("4", "4");
+        map.put("1",  "H");
+
+        //System.out.println("Map list " + map);
+//        System.out.println(map.get("A"));
+//        System.out.println(map.remove("A"));
+//        System.out.println(map.get("A"));
+        map.remove("1");
+        System.out.println(map);
+
+
+
+        //namer.addall(names);
 
 //        System.out.println(Arrays.toString());
 //        System.out.println(Arrays.toString(namer.arrSort()));
@@ -21,13 +43,13 @@ public class Main {
 //        System.out.println("hello world");
 
 //
-        LinkedListCustomized linkedListCustomized = new LinkedListCustomized();
-//
-        linkedListCustomized.addFirst(1);
-        linkedListCustomized.addLast(5);
-        linkedListCustomized.addLast(3);
-        linkedListCustomized.addLast(2);
-        linkedListCustomized.addLast(4);
+//        LinkedListCustomized linkedListCustomized = new LinkedListCustomized();
+////
+//        linkedListCustomized.addFirst(1);
+//        linkedListCustomized.addLast(5);
+//        linkedListCustomized.addLast(3);
+//        linkedListCustomized.addLast(2);
+//        linkedListCustomized.addLast(4);
 
 //        System.out.println(linkedListCustomized.get(2));
 //
@@ -45,16 +67,19 @@ public class Main {
 
 //        Iterator<Integer> iterator = Arrays.asList(linkedListCustomized.descendingIterator()).iterator();
 
-        Iterator<Integer> iterator = linkedListCustomized.descendingIterator();
-
-        while(iterator.hasNext()) {
-            System.out.println(iterator.next());
-        }
+//        Iterator<Integer> iterator = linkedListCustomized.descendingIterator();
+//
+//        while(iterator.hasNext()) {
+//            System.out.println(iterator.next());
+//        }
 
 //        System.out.println("hello world");
-        long endTime = System.currentTimeMillis();
-        long duration = endTime - startTime;
-        System.out.println("Time taken: " + duration + " milliseconds");
+//        long endTime = System.currentTimeMillis();
+//      //  long duration = endTime - startTime;
+//        System.out.println("Time taken: " + duration + " milliseconds");
     }
+
+    //CustomizedHashMap<String, String> customizedHashMap = new CustomizedHashMap<String, String>();
+
 
 }
